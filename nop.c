a8bf29927edb5b9bc5c849726dfd3a36be4c6fda
Rosemwende <rosemuema82@gmail.com>
2024-03-31 09:41:48 -0700

    nop.c

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * f_nop - it does not do anything
+ * @head: pointer at the head of stack
+ * @counter: line_number
+ * Return: void
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+(void) counter;
+(void) head;
+}
