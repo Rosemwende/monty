55b789846b8442de2d705826fa6a6e68c629e959
Rosemwende <rosemuema82@gmail.com>
2024-03-31 06:55:52 -0700

    pstr.c

---
+++ pstr.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+/**
+ * f_pstr - this prints the string starting at the
+ * top of the stack followed by a new line
+ * @head: pointer at the head of stack
+ * @counter: line_number
+ * Return: Void
+ */
+void f_pstr(stack_t **head, unsigned int counter)
+{
+stack_t *h;
+(void)counter;
+
+h = *head;
+while (h)
+{
+if (h->n > 120 || h->n <= 0)
+{
+break;
+}
+printf("%c", h->n);
+}
+printf("\n");
+}
