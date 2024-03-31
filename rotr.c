1fb769d1228251786f2c47544d406b468cc1b302
Rosemwende <rosemuema82@gmail.com>
2024-03-31 07:05:46 -0700

    rotr.c

---
+++ rotr.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+/**
+ * f_rotr - this rotates the stack to the bottom
+ * @head: pointer at the head of stack
+ * @counter: line_number
+ * Return: void
+ */
+void f_rotr(stack_t **head, _attribute_((unused)) unsigned int counter)
+{
+stack_t *copy;
+
+copy = *head;
+if (*head == NULL || (*head)->next == NULL)
+{
+return;
+}
+while (copy->next)
+{
+copy = copy->next;
+}
+copy->next = *head;
+copy->prev->next = NULL;
+copy->prev = NULL;
+(*head)->prev = copy;
+(*head) = copy;
+}
