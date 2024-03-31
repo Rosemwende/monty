2788824099339d54631fc781a54ee515c0af58a1
Rosemwende <rosemuema82@gmail.com>
2024-03-31 09:12:23 -0700

    rotr.c

--- rotr.c
+++ rotr.c
@@ -5,7 +5,7 @@
  * @counter: line_number
  * Return: void
  */
-void f_rotr(stack_t **head, _attribute_((unused)) unsigned int counter)
+void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter)
 {
 stack_t *copy;
 
